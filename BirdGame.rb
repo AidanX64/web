d3b5abeb634f48043884dbbcdb53f244a0dfc5cd
Aidan Armstrong <doomslayer122805@gmail.com>
2025-11-14 13:18:23 -0800

    Added BirdGame

---
+++ BirdGame.rb
@@ -0,0 +1,11 @@
+puts "Bird Game"
+
+print "Enter a Bird name: "
+
+Bird_name = gets.chomp
+
+print "Enter a Species name: "
+
+Species_name = gets.chomp
+
+puts "Your bird is a: " + Species_name.to_s + " named " + Bird_name.to_s
